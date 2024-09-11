5a683d57ef33850f4e6184b14bf7f17edd663a89
DavronbekMamarasulov07 <davronbekmamarasulov07@gmail.com>
2024-09-11 16:32:17 +0500

    update

--- src/components/expense-container/index.tsx
+++ src/components/expense-container/index.tsx
@@ -3,7 +3,7 @@ import ExpenseList from "./expense-list/ExpenseList"
 
 const index = () => {
   return (
-    <div className="max-w-[1000px] mx-auto shadow-2xl flex-1 p-10 max-h-[500px]">
+    <div className="max-w-[1200px] mx-auto shadow-2xl flex-1 p-10 max-h-[500px]">
         <div className="flex justify-between items-start gap-10">
         <ExpenseList/>
         <ExpenseDisplay/>
